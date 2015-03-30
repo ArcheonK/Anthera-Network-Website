010f078f0f635cb3eea9d9d2935b2a158180619a
Killazombiecow <sean@trintondesigns.com>
2015-03-29 23:57:53 -0400

    Added some more stuff

--- server.js
+++ server.js
@@ -1,24 +1,128 @@
 //var subdomain = require('express-subdomain');
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'prosperity1',
+	database: 'anthera'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM `people` WHERE name="Sean"', function(err, results) {
+	if(err) throw err;
+	//console.log(results[0].name);
+});
+
+connection.end();
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/views/assets'));
 
- var menuItems = [
-	{ link: 'http://google.com', name: 'Home', },
-	{ link: 'http://google.com', name: 'Forums' },
-	{ link: 'http://google.com', name: 'Store', },
-	{ link: 'http://google.com', name: 'Stats', },
-	{ link: '#', name: '<img src="/images/GoA-logo.png" alt="Logo" id="nav-logo">' },
-	{ link: 'http://google.com', name: 'Bans',  },
-	{ link: 'http://google.com', name: 'Map', },
-	{ link: 'http://google.com', name: 'Servers', },
-	{ link: 'http://google.com', name: 'Apply', }
-];
+app.use(function(req, res, next) {
+	console.log(req.connection.remoteAddress + ' has connected');
+	next();
+});
 
-var menuItemActive = 'active';
+app.get('/', function(req, res) {
+	var menuItems = [
+		{ link: '/', name: 'Home', active: 'active' },
+		{ link: 'http://google.com', name: 'Forums', active: '' },
+		{ link: '/store', name: 'Store', active: '' },
+		{ link: 'http://google.com', name: 'Stats', active: '' },
+		{ link: 'http://google.com', name: 'Bans', active: ''  },
+		{ link: 'http://google.com', name: 'Map', active: '' },
+		{ link: 'http://google.com', name: 'Servers', active: '' },
+		{ link: 'http://google.com', name: 'Apply', active: '' }
+	];
+
+	res.render('pages/index', {
+		menuItems: menuItems
+	});
+});
+
+app.get('/store', function(req, res) {
+	var menuItems = [
+		{ link: '/', name: 'Home', active: '' },
+		{ link: 'http://google.com', name: 'Forums', active: 'a' },
+		{ link: '/store', name: 'Store', active: 'active' },
+		{ link: 'http://google.com', name: 'Stats', active: '' },
+		{ link: 'http://google.com', name: 'Bans', active: ''  },
+		{ link: 'http://google.com', name: 'Map', active: '' },
+		{ link: 'http://google.com', name: 'Servers', active: '' },
+		{ link: 'http://google.com', name: 'Apply', active: '' }
+	];
+
+	res.render('pages/store', {
+		menuItems: menuItems
+	});
+});
+
+// Debugging
+app.get('/404', function(req, res) {
+	res.status(404);
+});
+
+app.get('/403', function(req, res) {
+	res.status(403);
+});
+
+app.get('/500', function(req, res) {
+	res.status(500);
+});
+
+app.get('/400', function(req, res) {
+	res.status(400);
+});
+
+app.get('*', function(req, res, next) {
+	var err = new Error();
+	err.status = 404;
+	next(err);
+});
+
+app.use(function(err, req, res, next) {
+	if(err.status !== 404) {
+		return next();
+	} else if (err.status == 400) {
+		res.render('error/error', {
+			error: '400',
+			message: 'Bad Request'
+		});
+		console.log(err.status);
+	} else if (err.status == 403) {
+		res.render('error/error', {
+			error: '403',
+			message: 'You are not authorized to view this document'
+		});
+		console.log(err.status);
+	} else if (err.status == 500) {
+		res.render('error/error', {
+			error: '500',
+			message: 'Internal Server Error'
+		});
+		console.log(err.status);
+	} else {
+		res.render('error/error', {
+			error: '404',
+			message: 'The document you requested could not be found or is corrupted'
+		});
+		console.log(req.connection.remoteAddress + ' encountered a ' + err.status);
+	} 
+});
+
+var server = app.listen(3000, function() {
+	console.log('Anthera Website Running.');
+});
+
+/*
+app.use(subdomain('static', router));
+*/
+
 /*
 var router = express.Router();
 router.get('/css', function(req, res, next) {
@@ -47,23 +151,3 @@ createTable.save(function(err) {
 		console.log(createTable);
 })
 */
-
-
-app.use(function(req, res, next) {
-	console.log(req.connection.remoteAddress + ' has connected');
-	next();
-});
-
-app.get('/', function(req, res) {
-	res.render('pages/index', {
-		menuItems: menuItems ,
-		menuItemActive: menuItemActive
-	});
-});
-
-/*
-app.use(subdomain('static', router));
-*/
-var server = app.listen(3000, function() {
-	console.log('Anthera Website Running.');
-});
